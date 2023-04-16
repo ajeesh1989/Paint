189cc162f1d061e5df98a52060a63a71c7bb6e91
ajeesh1989 <ajeeshrko@gmail.com>
2023-04-16 16:55:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: DrawingScreen(),
+    );
+  }
+}
+
+class DrawingScreen extends StatefulWidget {
+  const DrawingScreen({super.key});
+
+  @override
+  _DrawingScreenState createState() => _DrawingScreenState();
+}
+
+class _DrawingScreenState extends State<DrawingScreen> {
+  Color _selectedColor = Colors.black;
+
+  List<Offset> _points = <Offset>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawing Board'),
+      ),
+      body: Container(
+        child: GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox renderBox = context.findRenderObject() as RenderBox;
+              Offset point = renderBox.globalToLocal(details.globalPosition);
+              _points = List.from(_points)..add(point);
+            });
+          },
+          onPanEnd: (DragEndDetails details) {
+            setState(() {
+              _points.add(Offset.zero);
+            });
+          },
+          child: CustomPaint(
+            painter: DrawingPainter(points: _points, color: _selectedColor),
+            size: Size.infinite,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Select a color'),
+                content: SingleChildScrollView(
+                  child: BlockPicker(
+                    pickerColor: _selectedColor,
+                    onColorChanged: (Color color) {
+                      setState(() {
+                        _selectedColor = color;
+                      });
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DrawingPainter extends CustomPainter {
+  final List<Offset> points;
+  final Color color;
+
+  DrawingPainter({required this.points, required this.color});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = color
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 5.0;
+
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) {
+    return oldDelegate.points != points || oldDelegate.color != color;
+  }
+}
